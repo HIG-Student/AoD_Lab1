c8d2e92790b48b083b9b6b38c325d5dc101c81bc
Viktor Hanstorp <ndi14vhp@student.hig.se>
2015-11-11 01:01:03 +0100

    Add base for CheckBalance
    Based on the one supplied by the teachers

---
+++ src/se/hig/aod/grupp/CheckBalance.java
@@ -0,0 +1,38 @@
+package se.hig.aod.grupp;
+
+/**
+ * This is free to use in courses at University of Gävle.
+ * 
+ * Authors of this exercise are:
+ * Magnus Hjelmblom
+ * Anders Jackson
+ */
+import se.hig.ad1.lab1.Stack;
+import se.hig.ad1.lab1.ListStack;
+
+/**
+ * A class that uses a {@link Stack} to check that an string has matched
+ * paranthesis.
+ */
+
+public class CheckBalance
+{
+    public static boolean isBalanced(String uttryck)
+    {
+
+        Stack stacken = new ListStack(); // Create a new stack
+
+        // Your code goes here
+        
+        return false;
+    }
+
+    public static void main(String[] args)
+    {
+
+        // Your code goes here
+        // Use java.util.Scanner to read lines from System.in
+
+    }
+
+} // end of class CheckBalance
